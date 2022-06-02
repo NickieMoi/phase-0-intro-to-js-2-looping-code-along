a752a3fdeec774a590722301c87b5dfd2279f5ab
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-02 13:43:43 +0300

    commit tree 38025e4f11f441bd29f678f46134ab140dfa7436

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(theArray, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < theArray.length; i++) {
+        thankYouCards.push(
+            `Thank you, ${theArray[i]}, for the wonderful ${event} gift!`
+        );
+    }
+    return thankYouCards;
+}
+function countdown(number) {
+    while (number > 0) {
+        console.log(number);
+        number-= 1;
+    }
+    console.log(number);
+}
